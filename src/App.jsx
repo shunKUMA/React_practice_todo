5e7ac3aed8e20f2c0ee918c562f673a5cdbb23c2
shunKUMA <kumachan4874@gmail.com>
2021-08-23 13:26:58 +0900

    新規リスト作成の上限設定

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,12 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>登録できるTodoは5個までです。</p>
+      )}
+
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
